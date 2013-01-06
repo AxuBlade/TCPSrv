2386c264ade24a1e90f818ce41e95e4f0d77b027
AxuBlade <jan@macheta.net>
2013-01-06 12:49:07 +0100

    update

---
+++ src/commands_handling/command_put.c
@@ -0,0 +1,45 @@
+#include "../defines.h"
+
+void command_put(struct cmdStruct* commandsList, int socket)  {
+
+  FILE* fileStream;
+  char writeBuffer[__BUFFER_SIZE];
+  int recvSize = 0;
+  int writeSize = 0;
+  int textModeCheck = !strncmp(commandsList->words[3],"-t",2) || !strncmp(commandsList->words[3],"--text",8);
+  int binaryModeCheck = !strncmp(commandsList->words[3],"-b",2) || !strncmp(commandsList->words[3],"--binary",8);
+  char* writeMode;
+  char* writeModeText = "wt";
+  char* writeModeBinary = "wr";
+  struct stat fileStat;
+
+  signal(SIGINT, SIG_IGN);
+  signal(SIGTERM, SIG_IGN);
+  if(commandsList->wordCount == 4 && (textModeCheck || binaryModeCheck))  {
+    if(textModeCheck) writeMode = writeModeText;
+    else if(binaryModeCheck) writeMode = writeModeBinary;
+    if((fileStream = fopen(commandsList->words[2],writeMode)) == NULL)  {
+      info_remote_handler(socket, __ERROR_OPENING_FILE);
+      shutdown(socket, 0);
+      close(socket);
+      exit(1);
+      }
+    else  {
+      while((recvSize = recv(socket, writeBuffer, __BUFFER_SIZE, 0)) > 0) {
+        writeSize = fwrite(writeBuffer, sizeof(char), recvSize, fileStream);
+        }
+      fclose(fileStream);
+      stat(commandsList->words[2], &fileStat);
+      if(!fileStat.st_size)  {
+        printf("Odebrano pusty plik plik %s -> usuwanie\n", commandsList->words[2], (signed int) fileStat.st_size);
+        info_remote_handler(socket,__EMPTY_FILE);
+        remove(commandsList->words[2]);
+        } else printf("Odebrano plik %s o rozmiarze %d\n", commandsList->words[2], (signed int) fileStat.st_size);
+      shutdown(socket,0);
+      close(socket);
+      exit(0);
+      }
+  } else info_remote_handler(socket,__PUT_WRONG_SYNTAX);
+
+
+}
